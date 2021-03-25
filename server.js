3a5167ef34c8bd3560fd9d7787378d9284270255
Marcelle Louise <marcellelouisek@gmail.com>
2021-03-25 11:56:52 +0000

    commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const ejs = require('ejs');
+const { kStringMaxLength } = require('buffer');
+
+
+//set your view engine as ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+// making a connection to mongoDB
+mongoose.connect('mongodb+srv://marcelle:letmein123@cluster0.oia29.mongodb.net/productsDB?retryWrites=true&w=majority');
+
+//creating a product Model
+const productsSchema = {
+    type: String,
+    description: String,
+    image: String,
+    category: String,
+    price: String
+}
+
+const Product = mongoose.model('Product', productsSchema);
+
+//getting all results in product and rendering
+app.get('/', (req, res) => {
+    Product.find({}, function (err, products) {
+        res.render('index')
+    })
+})
+
+//Getting all views routes
+app.get('/index', (req, res) => {
+    res.render('index');
+})
+
+app.get('/products', (req, res) => {
+    Product.find({}, function (err, products) {
+        res.render('products', {
+            productsList: products
+        })
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.get('/faqs', (req, res) => {
+    res.render('faqs');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+
+app.listen(8000, function () {
+    console.log('server is running');
+})
