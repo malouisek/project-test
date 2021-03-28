46810313350c1e6e66fbb8329e8baca386d0ee13
Marcelle Louise <marcellelouisek@gmail.com>
2021-03-28 20:17:21 +0000

    commit

--- public/js/faqs.js
+++ public/js/faqs.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
         $('#accordion-content-2').hide();
         $('#accordion-content-3').hide();
         $('#accordion-content-4').hide();
+        $('#accordion-content-5').hide();
         $('#up1').toggleClass('rotate');
         $('#up2').removeClass('rotate');
         $('#up3').removeClass('rotate');
         $('#up4').removeClass('rotate');
+        $('#up5').removeClass('rotate');
     });
 
     $('#accordion-button-2').on('click', () => {
@@ -16,10 +18,12 @@ $(document).ready(function(){
         $('#accordion-content-1').hide();
         $('#accordion-content-3').hide();
         $('#accordion-content-4').hide();
+        $('#accordion-content-5').hide();
         $('#up2').toggleClass('rotate');
         $('#up1').removeClass('rotate');
         $('#up3').removeClass('rotate');
         $('#up4').removeClass('rotate');
+        $('#up5').removeClass('rotate');
     })
 
     $('#accordion-button-3').on('click', () => {
@@ -27,10 +31,12 @@ $(document).ready(function(){
         $('#accordion-content-2').hide();
         $('#accordion-content-1').hide();
         $('#accordion-content-4').hide();
+        $('#accordion-content-5').hide();
         $('#up3').toggleClass('rotate');
         $('#up2').removeClass('rotate');
         $('#up1').removeClass('rotate');
         $('#up4').removeClass('rotate');
+        $('#up5').removeClass('rotate');
     })
 
     $('#accordion-button-4').on('click', () => {
@@ -38,11 +44,24 @@ $(document).ready(function(){
         $('#accordion-content-2').hide();
         $('#accordion-content-3').hide();
         $('#accordion-content-1').hide();
+        $('#accordion-content-5').hide();
         $('#up4').toggleClass('rotate');
         $('#up2').removeClass('rotate');
         $('#up3').removeClass('rotate');
         $('#up1').removeClass('rotate');
+        $('#up5').removeClass('rotate');
     })
-
-
+    
+    $('#accordion-button-5').on('click', () => {
+        $('#accordion-content-5').toggle();
+        $('#accordion-content-2').hide();
+        $('#accordion-content-3').hide();
+        $('#accordion-content-1').hide();
+        $('#accordion-content-4').hide();
+        $('#up5').toggleClass('rotate');
+        $('#up2').removeClass('rotate');
+        $('#up3').removeClass('rotate');
+        $('#up1').removeClass('rotate');
+        $('#up5').removeClass('rotate');
+    })
 })
